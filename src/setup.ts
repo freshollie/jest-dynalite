fce09c85d215e597cd5d737374232485a9689dc2
Roy Razon <roy.razon@salto.io>
2019-12-08 13:15:29 +0200

    fix: do not override existing AWS credentials in env

--- src/setup.ts
+++ src/setup.ts
@@ -10,6 +10,11 @@ export default (withConfigDir: string): void => {
   process.env.MOCK_DYNAMODB_ENDPOINT = `localhost:${port}`;
 
   // aws-sdk requires access and secret key to be able to call DDB
-  process.env.AWS_ACCESS_KEY_ID = "access-key";
-  process.env.AWS_SECRET_ACCESS_KEY = "secret-key";
+  if (!process.env.AWS_ACCESS_KEY_ID) {
+    process.env.AWS_ACCESS_KEY_ID = "access-key";
+  }
+
+  if (!process.env.AWS_SECRET_ACCESS_KEY) {
+    process.env.AWS_SECRET_ACCESS_KEY = "secret-key";
+  }
 };
