bb813ed95f6a5494bf53c91761231e04ef78e0e3
Paweł Tekliński <pawel.teklinski@schibsted.com>
2022-04-27 09:44:36 +0200

    chore: mock config file in unit tests for config

--- src/__testdir__/defined-basePort/jest-dynalite-config.ts
+++
@@ -1,7 +0,0 @@
-export const BASE_PORT = 10500;
-
-// fake config
-export default {
-  tables: [],
-  basePort: BASE_PORT,
-};
--- src/config.spec.ts
+++ src/config.spec.ts
@@ -1,39 +1,55 @@
-import { join } from "path";
+import fs from "fs";
+import { Config } from "./types";
 import { getDynalitePort, setConfigDir } from "./config";
-import { BASE_PORT } from "./__testdir__/defined-basePort/jest-dynalite-config";
 
+const BASE_PORT = 8443;
 const DEFAULT_BASE_PORT = 8000;
 
-describe("Config", () => {
-  const configDirectory = join(__dirname, "__testdir__", "defined-basePort");
+const mockedConfig = jest.fn((): Config => ({ basePort: BASE_PORT }));
+const configPath = "/fakepath/jest-dynalite-config.js";
 
+jest.mock("fs");
+(fs.existsSync as jest.Mock).mockReturnValue(true);
+
+jest.mock("path", () => {
+  const original = jest.requireActual("path");
+  return {
+    ...original,
+    resolve: jest.fn(() => configPath),
+  };
+});
+
+jest.mock(configPath, () => mockedConfig(), { virtual: true });
+
+describe("Config", () => {
   beforeAll(() => {
-    setConfigDir(configDirectory);
+    setConfigDir("/whatever");
   });
 
-  it("a different port is returned for each worker", () => {
+  test("a different port is returned for each worker", () => {
     const expectedPort =
       BASE_PORT + parseInt(process.env.JEST_WORKER_ID as string, 10);
 
     expect(getDynalitePort()).toBe(expectedPort);
   });
 
-  it("should return dynalite port even there is no JEST_WORKER_ID", () => {
+  test("should return dynalite port even there is no JEST_WORKER_ID", () => {
     const workerId = process.env.JEST_WORKER_ID;
     delete process.env.JEST_WORKER_ID;
 
     const port = getDynalitePort();
 
     expect(port).not.toBeNaN();
-    expect(getDynalitePort()).toBe(BASE_PORT);
+    expect(port).toBe(BASE_PORT);
 
     process.env.JEST_WORKER_ID = workerId;
   });
 
-  it("if basePort is not defined then port 8000 will be used as a default", () => {
-    setConfigDir(join(__dirname, "__testdir__"));
+  test("if basePort is not defined then port 8000 will be used as a default", () => {
+    jest.resetModules();
+    mockedConfig.mockReturnValue({});
     const expectedPort =
-      DEFAULT_BASE_PORT + parseInt(process.env.JEST_WORKER_ID as string, 10);
+      DEFAULT_BASE_PORT + parseInt(process.env.JEST_WORKER_ID || "0", 10);
 
     expect(getDynalitePort()).toBe(expectedPort);
   });
